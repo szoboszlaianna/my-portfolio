652e7d68e966b01e5fdf7f61bf770ed8209dad48
szoboszlaianna <szoboszlai.anna0@gmail.com>
2021-10-25 16:21:17 +0200

    Netify,projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,31 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message })
+    })
+      .then(() => alert("Message sent!"))
+      .catch(error => alert(error));
+  }
+
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-around">
-        <form netlify name="contact" className="lg:w-1/2 md:w-1/2 flex flex-col w-full p-20">
+        <form netlify name="contact" className="lg:w-1/2 md:w-1/2 flex flex-col w-full p-20" onSubmit={handleSubmit}>
           <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">Contact Me</h2>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm">
