668656ed619f79d21c446faed4dfc48a49c2c477
szoboszlaianna <szoboszlai.anna0@gmail.com>
2021-10-30 17:12:30 +0200

    add modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,46 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import Confirmation from "./Confirmation";
+import React from 'react';
+import Confirmation from './Confirmation';
 
 export default function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [submited, setSubmited] = React.useState(false);
 
   function encode(data) {
     return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message })
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', name, email, message }),
     })
       .then(() => {
-        alert("Message sent!");
-        setMessage("");
-        setEmail("");
-        setName("");
+        setMessage('');
+        setEmail('');
+        setName('');
+        setSubmited(true);
       })
       .catch(error => alert(error));
   }
 
+  function closeModal() {
+    setSubmited(false);
+  }
+
   return (
-    <section id="contact" className="relative">
-      <Confirmation />
-      <div className="container px-5 py-5 mx-auto flex sm:flex-nowrap flex-wrap justify-around">
-        <form netlify name="contact" className="lg:w-1/2 md:w-1/2 flex flex-col w-full p-20" onSubmit={handleSubmit}>
-          <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">Contact Me</h2>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm">
+    <section id='contact' className='relative'>
+      {submited ? <Confirmation onClose={closeModal} /> : null}
+      <div className='container px-5 py-5 mx-auto flex sm:flex-nowrap flex-wrap justify-around'>
+        <form netlify name='contact' className='lg:w-1/2 md:w-1/2 flex flex-col w-full p-20' onSubmit={handleSubmit}>
+          <h2 className='sm:text-4xl text-3xl mb-1 font-medium title-font'>Contact Me</h2>
+          <div className='relative mb-4'>
+            <label htmlFor='name' className='leading-7 text-sm'>
               Name
             </label>
             <input
@@ -46,29 +48,31 @@ export default function Contact() {
                 setName(e.target.value);
               }}
               value={name}
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              type='text'
+              id='name'
+              name='name'
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+              required
             />
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm">
+          <div className='relative mb-4'>
+            <label htmlFor='email' className='leading-7 text-sm'>
               Email
             </label>
             <input
               onChange={e => {
                 setEmail(e.target.value);
               }}
-              type="email"
+              type='email'
               value={email}
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              id='email'
+              name='email'
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+              required
             />
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="message" className="leading-7 text-sm">
+          <div className='relative mb-4'>
+            <label htmlFor='message' className='leading-7 text-sm'>
               Message
             </label>
             <textarea
@@ -76,12 +80,13 @@ export default function Contact() {
                 setMessage(e.target.value);
               }}
               value={message}
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              id='message'
+              name='message'
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
+              required
             />
           </div>
-          <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg lg:w-1/3 md:w-1/2">
+          <button type='submit' className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg lg:w-1/3 md:w-1/2'>
             Submit
           </button>
         </form>
--- src/data.js
+++ src/data.js
@@ -1,34 +1,34 @@
 export const projects = [
   {
-    title: "Guess the number app",
-    subtitle: "React-native",
-    description: "The app guesses the number. ",
-    image: "./project_react-native.gif",
-    link: "https://github.com/szoboszlaianna/guess_the_number"
+    title: 'Guess the number app',
+    subtitle: 'React-native',
+    description: 'The app guesses the number the user entered. The user should give feedback after guesses if the number was lower/greater.',
+    image: './project_react-native.gif',
+    link: 'https://github.com/szoboszlaianna/guess_the_number',
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com"
-  }
+    title: 'Expense Calculator',
+    subtitle: 'React',
+    description: 'Simple react project. New expenses can be added using the form, and the chart shows the expenses for the month for that given year.',
+    image: './project-expense_calculator.gif',
+    link: 'https://github.com/szoboszlaianna/expense-calculator',
+  },
 ];
 
 export const testimonials = [
   {
-    quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle"
+    quote: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/men/1.jpg',
+    name: 'Jesse Hicks',
+    company: 'Zoozle',
   },
   {
-    quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io"
-  }
+    quote: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/men/94.jpg',
+    name: 'Ruben Alvarez',
+    company: 'Dooble.io',
+  },
 ];
 
-export const skills = ["JavaScript", "React", "Node", "Material UI", "Bootstrap", "HTML&CSS", "SQL", "Git", "Python", "Pandas", "Data Science", "Deep learning", "OO paradigms", "Typescript"];
-export const light_skills = ["Angular", "React-native", "Redux", "Docker", "Elasticsearch", "Logstash", "Kibana", "Java"];
+export const skills = ['JavaScript', 'React', 'Node', 'Material UI', 'Bootstrap', 'HTML&CSS', 'SQL', 'Git', 'Python', 'Pandas', 'Data Science', 'Deep learning', 'OO paradigms', 'Typescript'];
+export const light_skills = ['Angular', 'React-native', 'Redux', 'Docker', 'Elasticsearch', 'Logstash', 'Kibana', 'Java'];
