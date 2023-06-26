a3dec5f4ee2794c096afd6329cf936851e7a2da1
Anna Szoboszlai <annaszoboszlai@Annas-Air.lan>
2023-06-26 19:20:51 +0200

    remove node modules, add new project

--- src/data.js
+++ src/data.js
@@ -1,34 +1,72 @@
 export const projects = [
   {
-    title: 'Guess the number app',
-    subtitle: 'React-native',
-    description: 'The app guesses the number the user entered. The user should give feedback after guesses if the number was lower/greater.',
-    image: './project_react-native.gif',
-    link: 'https://github.com/szoboszlaianna/guess_the_number',
+    title: "PDF magic (in progress)",
+    subtitle: "React",
+    description:
+      "PDF Magic is an application that provides various PDF functionalities, such as merging PDFs and deleting pages etc. Currently the merge funcionality is implemented.",
+    image: "./pdfmagicdemo.gif",
+    link: "https://github.com/szoboszlaianna/pdfmagic",
   },
   {
-    title: 'Expense Calculator',
-    subtitle: 'React',
-    description: 'Simple react project. New expenses can be added using the form, and the chart shows the expenses for the month for that given year.',
-    image: './project-expense_calculator.gif',
-    link: 'https://github.com/szoboszlaianna/expense-calculator',
+    title: "Guess the number app",
+    subtitle: "React-native",
+    description:
+      "The app guesses the number the user entered. The user should give feedback after guesses if the number was lower/greater.",
+    image: "./project_react-native.gif",
+    link: "https://github.com/szoboszlaianna/guess_the_number",
+  },
+  {
+    title: "Expense Calculator",
+    subtitle: "React",
+    description:
+      "Simple react project. New expenses can be added using the form, and the chart shows the expenses for the month for that given year.",
+    image: "./project-expense_calculator.gif",
+    link: "https://github.com/szoboszlaianna/expense-calculator",
   },
 ];
 
 export const testimonials = [
   {
-    quote: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
-    image: 'https://randomuser.me/api/portraits/men/1.jpg',
-    name: 'Jesse Hicks',
-    company: 'Zoozle',
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
   },
   {
-    quote: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
-    image: 'https://randomuser.me/api/portraits/men/94.jpg',
-    name: 'Ruben Alvarez',
-    company: 'Dooble.io',
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
   },
 ];
 
-export const skills = ['JavaScript', 'React', 'Node', 'Material UI', 'Bootstrap', 'HTML&CSS', 'SQL', 'Git', 'Python', 'Pandas', 'Data Science', 'Deep learning', 'OO paradigms', 'Typescript'];
-export const light_skills = ['Angular', 'React-native', 'Redux', 'Docker', 'Elasticsearch', 'Logstash', 'Kibana', 'Java'];
+export const skills = [
+  "Typescript",
+  "React",
+  "Node",
+  "Material UI",
+  "Python",
+  "Fast API",
+  "Flask",
+  "HTML&CSS",
+  "Postgress",
+  "SQL",
+  "Git",
+  "Pandas",
+  "Data Science",
+  "Deep learning",
+  "OO paradigms",
+  "Javascript",
+];
+export const light_skills = [
+  "Angular",
+  "React-native",
+  "Redux",
+  "Docker",
+  "Elasticsearch",
+  "Logstash",
+  "Kibana",
+  "Java",
+];
